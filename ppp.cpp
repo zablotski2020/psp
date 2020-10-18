2c28020629370821de60b742c1dd891f7e527b59
zablotski2020 <72109229+zablotski2020@users.noreply.github.com>
2020-10-19 00:37:55 +0300

    IHGIU

---
+++ ppp.cpp
@@ -0,0 +1,7 @@
+#include "ppp.hpp"
+#include <iostream>
+usig namespace std;
+
+int main{
+    cout << "HGHG" << endl;
+}
---
+++ ppp.hpp
@@ -0,0 +1,13 @@
+//
+//  ppp.hpp
+//  
+//
+//  Created by Ilya on 10/19/20.
+//
+
+#ifndef ppp_hpp
+#define ppp_hpp
+
+#include <stdio.h>
+
+#endif /* ppp_hpp */
